09c248036104e0e94cf6cdb919d8dabf250cb7ee
masaki.yamakawa <masaki.yamakawa@ulsystems.co.jp>
2021-06-27 11:03:46 +0900

    Impl roles function

---
+++ RolesFunction/src/RolesFunction/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RolesFunction
+{
+    public class Role
+    {
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public List<string> PrivilegeTypes { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public DateTime UpdatedTimestamp { get; set; }
+    }
+}
